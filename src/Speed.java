5c73fe4ca4e2741549aa977643d623567cf7da42
Xaga97 <jd.correa@uniandes.edu.co>
2018-03-25 14:21:56 -0500

    Response Handler agregado
    Para que tire exception si no es un 200

--- src/Speed.java
+++ src/Speed.java
@@ -4,14 +4,35 @@ import java.io.InputStream;
 import org.apache.http.Consts;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
+import org.apache.http.StatusLine;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.HttpResponseException;
+import org.apache.http.client.ResponseHandler;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.ContentType;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.HttpClients;
 
 public class Speed {
+	ResponseHandler<String> rh = new ResponseHandler<String>() {
+
+	    @Override
+	    public String handleResponse(
+	            final HttpResponse response) throws IOException {
+	        StatusLine statusLine = response.getStatusLine();
+	        HttpEntity entity = response.getEntity();
+	        if (statusLine.getStatusCode() != 200) {
+	            throw new HttpResponseException(
+	                    statusLine.getStatusCode(),
+	                    statusLine.getReasonPhrase());
+	        }
+	        if (entity == null) {
+	            throw new ClientProtocolException("Response contains no content");
+	        }
+			return null;
+	    }
+	};
 	public Speed(int asd) throws ClientProtocolException, IOException {
 	HttpClient httpclient = HttpClients.createDefault();
 	HttpPost httppost = new HttpPost("http://www.a-domain.com/foo/");
@@ -25,17 +46,6 @@ public class Speed {
 	else {a="ALERTA 2 = SENSOR DE PRESENCIA ACTIVADO";}
 	httppost.setEntity((HttpEntity) new StringEntity(a,
 	        ContentType.create("plain/text", Consts.UTF_8)));
-
-	//Execute and get the response.
-	HttpResponse response = httpclient.execute(httppost);
-	HttpEntity entity = response.getEntity();
-
-	if (entity != null) {
-	    InputStream instream = entity.getContent();
-	    try {
-	    } finally {
-	        instream.close();
-	    }
-	}
+	httpclient.execute(httppost, rh);
 }
 }
