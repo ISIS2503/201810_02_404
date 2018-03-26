de70ecc0f989f0b318091a0df0f57573639b6ec9
MILLER ANDRES TRUJILLO ACHURY <ma.trujillo10@sis.virtual.uniandes.edu.co>
2018-03-25 19:39:19 -0500

    Capa speed

--- src/lambda/SimpleMqttClient.java
+++ src/lambda/SimpleMqttClient.java
@@ -6,23 +6,19 @@ import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
 import org.eclipse.paho.client.mqttv3.MqttCallback;
 import org.eclipse.paho.client.mqttv3.MqttClient;
 import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
-import org.eclipse.paho.client.mqttv3.MqttDeliveryToken;
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
-import org.eclipse.paho.client.mqttv3.MqttTopic;
 
 public class SimpleMqttClient implements MqttCallback {
 
-  MqttClient myClient;
+	MqttClient myClient;
 	MqttConnectOptions connOpt;
 
-	static final String BROKER_URL = "172.24.42.107:8083";
-	static final String M2MIO_USERNAME = "<m2m.io username>";
-	static final String M2MIO_PASSWORD_MD5 = "<m2m.io password (MD5 sum of password)>";
+	static final String BROKER_URL = "tcp://localhost:8083";
 
 	// the following two flags control whether this example is a publisher, a subscriber or both
 	static final Boolean subscriber = true;
-	static final Boolean publisher = true;
+	static final Boolean publisher = false;
 
 	/**
 	 * 
@@ -33,54 +29,6 @@ public class SimpleMqttClient implements MqttCallback {
 	@Override
 	public void connectionLost(Throwable t) {
 		System.out.println("Connection lost!");
-		// code to reconnect to the broker would go here if desired
-	}
-
-	/**
-	 * 
-	 * deliveryComplete
-	 * This callback is invoked when a message published by this client
-	 * is successfully received by the broker.
-	 * 
-	 */
-	public void deliveryComplete(MqttDeliveryToken token) {
-		//System.out.println("Pub complete" + new String(token.getMessage().getPayload()));
-	}
-
-	/**
-	 * 
-	 * messageArrived
-	 * This callback is invoked when a message is received on a subscribed topic.
-	 * 
-	 */
-	public void messageArrived(MqttTopic topic, MqttMessage message) throws Exception {
-		String payload = new String(message.getPayload());
-		System.out.println("-------------------------------------------------");
-		System.out.println("| Topic:" + topic.getName());
-		System.out.println("| Message: " + payload);
-		System.out.println("-------------------------------------------------");
-		// Procesar el mensaje y darle el formato
-		String data = "";
-		// Recibe el mensaje y lo envía al mock de manera asincrona
-		new Thread(new Runnable() {
-			@Override
-			public void run() {
-				try {
-					new Speed(data);
-				} catch (ClientProtocolException e) {
-					e.printStackTrace();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-		});
-		// Persiste la información, ojalá de manera asincrona
-		new Thread(new Runnable() {
-			@Override
-			public void run() {
-				new Batch(data);
-			}
-		});
 	}
 
 	/**
@@ -92,7 +40,7 @@ public class SimpleMqttClient implements MqttCallback {
 		SimpleMqttClient smc = new SimpleMqttClient();
 		smc.runClient();
 	}
-	
+
 	/**
 	 * 
 	 * runClient
@@ -104,12 +52,10 @@ public class SimpleMqttClient implements MqttCallback {
 		// setup MQTT Client
 		String clientID = "apto01";
 		connOpt = new MqttConnectOptions();
-		
+
 		connOpt.setCleanSession(true);
 		connOpt.setKeepAliveInterval(30);
-		connOpt.setUserName(M2MIO_USERNAME);
-		connOpt.setPassword(M2MIO_PASSWORD_MD5.toCharArray());
-		
+
 		// Connect to Broker
 		try {
 			myClient = new MqttClient(BROKER_URL, clientID);
@@ -119,13 +65,12 @@ public class SimpleMqttClient implements MqttCallback {
 			e.printStackTrace();
 			System.exit(-1);
 		}
-		
+
 		System.out.println("Connected to " + BROKER_URL);
 
 		// setup topic
 		// topics on m2m.io are in the form <domain>/<stuff>/<thing>
-		String myTopic = "propietario.apto1.alerta";
-		MqttTopic topic = myClient.getTopic(myTopic);
+		String myTopic = "alerta.apto1";
 
 		// subscribe to topic if subscriber
 		if (subscriber) {
@@ -138,35 +83,33 @@ public class SimpleMqttClient implements MqttCallback {
 		}
 
 		// publish messages if publisher
-		if (publisher) {
+		/*if (publisher) {
 			for (int i=1; i<=10; i++) {
-		   		String pubMsg = "{\"pubmsg\":" + i + "}";
-		   		int pubQoS = 0;
+				String pubMsg = "{\"pubmsg\":" + i + "}";
+				int pubQoS = 0;
 				MqttMessage message = new MqttMessage(pubMsg.getBytes());
-		    	message.setQos(pubQoS);
-		    	message.setRetained(false);
+				message.setQos(pubQoS);
+				message.setRetained(false);
 
-		    	// Publish the message
-		    	System.out.println("Publishing to topic \"" + topic + "\" qos " + pubQoS);
-		    	MqttDeliveryToken token = null;
-		    	try {
-		    		// publish message to broker
+				// Publish the message
+				System.out.println("Publishing to topic \"" + topic + "\" qos " + pubQoS);
+				MqttDeliveryToken token = null;
+				try {
+					// publish message to broker
 					token = topic.publish(message);
-			    	// Wait until the message has been delivered to the broker
+					// Wait until the message has been delivered to the broker
 					token.waitForCompletion();
 					Thread.sleep(100);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
 			}			
-		}
-		
+		}*/
+
 		// disconnect
 		try {
 			// wait to ensure subscribed messages are delivered
-			if (subscriber) {
-				Thread.sleep(5000);
-			}
+			while (subscriber);
 			myClient.disconnect();
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -175,13 +118,35 @@ public class SimpleMqttClient implements MqttCallback {
 
 	@Override
 	public void messageArrived(String topic, MqttMessage message) throws Exception {
-		// TODO Auto-generated method stub
-		
+		String payload = new String(message.getPayload());
+		System.out.println("-------------------------------------------------");
+		System.out.println("| Topic:" + topic);
+		System.out.println("| Message: " + payload);
+		System.out.println("-------------------------------------------------");
+		// Recibe el mensaje y lo envía al mock de manera asincrona
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					new Speed(payload);
+				} catch (ClientProtocolException e) {
+					e.printStackTrace();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}).start();
+		// Persiste la información, ojalá de manera asincrona
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				new Batch(payload);
+			}
+		});
 	}
 
 	@Override
 	public void deliveryComplete(IMqttDeliveryToken token) {
-		// TODO Auto-generated method stub
-		
+
 	}
 }
