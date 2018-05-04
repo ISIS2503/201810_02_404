f482b4861f6e491e1b227f8c15fc4faf0f4ac48d
jd.correa <jd.correa@ISIS2503S2-0013.sis.virtual.uniandes.edu.co>
2018-05-03 23:03:33 -0500

    Hecho el CRUD de cerraduras y HUBs

---
+++ CapaBatch/src/main/java/logic/CerraduraLogic.java
@@ -0,0 +1,70 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2018 Universidad De Los Andes - Departamento de Ingeniería de Sistemas.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package logic;
+import interfaces.ICerraduraLogic;
+import java.util.LinkedList;
+import static model.dto.converter.CerraduraConverter.CONVERTER;
+import model.dto.model.CerraduraDTO;
+import persistence.CerraduraPersistence;
+import java.util.List;
+import java.util.UUID;
+import model.entity.CerraduraEntity;
+
+public class CerraduraLogic implements ICerraduraLogic {
+
+    private final CerraduraPersistence persistence;
+
+    public CerraduraLogic() {
+        this.persistence = new CerraduraPersistence();
+    }
+
+    @Override
+    public CerraduraDTO add(CerraduraDTO dto) {
+        if (dto.getId() == null) {
+            dto.setId(UUID.randomUUID().toString());
+        }
+        return CONVERTER.entityToDto(persistence.add(CONVERTER.dtoToEntity(dto)));
+    }
+
+    @Override
+    public CerraduraDTO update(CerraduraDTO dto) {
+        CerraduraDTO result = CONVERTER.entityToDto(persistence.update(CONVERTER.dtoToEntity(dto)));
+        return result;
+    }
+
+    @Override
+    public CerraduraDTO find(String id) {
+        return CONVERTER.entityToDto(persistence.find(id));
+    }
+
+    @Override
+    public List<CerraduraDTO> all() {
+        return CONVERTER.listEntitiesToListDTOs(persistence.all());
+    }
+
+    @Override
+    public Boolean delete(String id) {
+        return persistence.delete(id);
+    }
+}
