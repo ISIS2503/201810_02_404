341851fa2bc9bc5c412fa7a73f31788047ccf88c
Xaga97 <jd.correa@uniandes.edu.co>
2018-05-11 08:57:07 -0500

    Al fin conecta Email

--- CapaSpeed/src/com/isi2503/speed/EmailSenderService.java
+++ CapaSpeed/src/com/isi2503/speed/EmailSenderService.java
@@ -19,29 +19,36 @@ public class EmailSenderService {
  
 		properties.put("mail.smtp.host", "smtp.gmail.com");
 		properties.put("mail.smtp.starttls.enable", "true");
-		properties.put("mail.smtp.port",587);
+		properties.put("mail.smtp.port","587");
 		properties.put("mail.smtp.mail.sender","xuaxy9@gmail.com");
 		properties.put("mail.smtp.user", "Yale");
 		properties.put("mail.smtp.auth", "true");
  
 		session = Session.getDefaultInstance(properties);
 	}
- 
+	public static void main(String[] args) {
+		EmailSenderService a = new EmailSenderService();
+		a.sendEmail("Prueba");
+	}
 	public void sendEmail(String x){
  
 		init();
 		try{
 			MimeMessage message = new MimeMessage(session);
 			message.setFrom(new InternetAddress((String)properties.get("mail.smtp.mail.sender")));
+			System.out.println("entra");
 			message.addRecipient(Message.RecipientType.TO, new InternetAddress("andsnake22@gmail.com"));
 			message.setSubject("Prueba");
 			message.setText(x);
 			Transport t = session.getTransport("smtp");
-			t.connect((String)properties.get("mail.smtp.user"), "CambiarAClaveAlPresentar");
+			System.out.println("entra2");
+			t.connect((String)properties.get("mail.smtp.host"),"xuaxy9@gmail.com", "CambiarAClaveEnPrivado");
+			System.out.println("conecta");
 			t.sendMessage(message, message.getAllRecipients());
 			t.close();
+			System.out.println("enviado");
 		}catch (MessagingException me){
-                        System.out.println("Error al enviar");
+                        System.out.println("Error al enviar,"+me.getMessage()+me.getClass().getName());
 			return;
 		}
 		
